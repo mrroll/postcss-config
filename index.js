1d47d2fdfce6f20e713ddd334bf631930bd1b263
mrroll <58090032+mrroll@users.noreply.github.com>
2022-04-12 00:30:22 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,18 @@
+// If you want to use other PostCSS plugins, see the following:
+// https://tailwindcss.com/docs/using-with-preprocessors
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      stage: 3,
+      autoprefixer: {
+        flexbox: "no-2009",
+      },
+      features: {
+        "custom-properties": false,
+      },
+    },
+    "postcss-discard-comments": { removeAll: true },
+  },
+};
